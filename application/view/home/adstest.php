f0557f7340feb831d1e7946981ef7232626a7eb8
Jakub Godawa <jakub.godawa@gmail.com>
2018-01-30 11:04:17 +0100

    ad test

--- application/view/home/adstest.php
+++ application/view/home/adstest.php
@@ -9,17 +9,15 @@
     <link rel="stylesheet" type="text/css" href="<?php echo Config::get('URL'); ?>css/slick-theme.css?v=<?php echo filemtime("css/style.css")?>" />
 
     <!-- Google Tag Manager -->
-
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-
-    })(window,document,'script','dataLayer','GTM-PRZYKLAD');</script>
-
+    <script>
+      var dataLayer = [];
+      dataLayer.push({"page": "home"});
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PWFPLTN');
+    </script>
     <!-- End Google Tag Manager -->
 </head>
 <body>
