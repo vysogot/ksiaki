aa189f3d2cf39ca9a3e67a9dd2361f1caeb0d1e5
Jakub Godawa <jakub.godawa@gmail.com>
2018-02-14 09:40:01 +0100

    updateContest procedure last inserted id

--- application/_installation/ksiaki-procedures_contest.sql
+++ application/_installation/ksiaki-procedures_contest.sql
@@ -78,44 +78,6 @@ DELIMITER ;
 
 -- Zrzut struktury procedura ksiaki.sp_updateContest
 DELIMITER //
-CREATE PROCEDURE `sp_createContest`(
-	IN `p_game_id` INT,
-	IN `p_contest_type_id` INT,
-	IN `p_name` VARCHAR(50),
-	IN `p_description` VARCHAR(50),
-	IN `p_banner_url` VARCHAR(50),
-	IN `p_begins_at` DATETIME,
-	IN `p_ends_at` DATETIME,
-	IN `p_display_ad` TINYINT(4),
-	OUT LID INT
-)
-BEGIN
-INSERT INTO _contests(
-	game_id,
-	contest_type_id,
-	name,
-	description,
-	banner_url,
-	begins_at,
-	ends_at,
-	display_ad
-) VALUES (
-	p_game_id,
-  p_contest_type_id,
-  p_name,
-  p_description,
-  p_banner_url,
-  p_begins_at,
-  p_ends_at,
-  p_display_ad
-);
-SET LID = LAST_INSERT_ID();
-SELECT ROW_COUNT() AS rowCount;
-END//
-DELIMITER ;
-
--- Zrzut struktury procedura ksiaki.sp_updateContest
-DELIMITER //
 CREATE PROCEDURE `sp_updateContest`(
 	IN `p_id` INT,
 	IN `p_game_id` INT,
@@ -139,9 +101,10 @@ SET game_id = p_game_id
 	, banner_url = p_banner_url
 	, begins_at = p_begins_at
 	, ends_at = p_ends_at
-	, display_ad = p_display_ad
+    , display_ad = p_display_ad
 WHERE (id = CASE WHEN (p_id = 0) THEN  LAST_INSERT_ID() ELSE p_id END);
-SELECT ROW_COUNT() AS rowCount;
+SELECT ROW_COUNT() AS rowCount, LAST_INSERT_ID() AS lastInsertId;
+END
 END//
 DELIMITER ;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
