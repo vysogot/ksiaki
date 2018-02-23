2ec023cc5a14eefd46758bbff711aa7915b26939
Jakub Godawa <jakub.godawa@gmail.com>
2018-02-23 18:37:25 +0100

    seeds

--- _installation/_seed.sql
+++ _installation/_seed.sql
@@ -4,14 +4,14 @@ DANE TESTOWE
 ------------
 ***********/
 
-DELETE FROM `_user`;
+TRUNCATE TABLE `_users`;
 INSERT INTO `_users`
   (`id`, `role_id`, `session_id`, `name`, `email`, `password_hash`, `active`, `activation_hash`, `created_at`, `last_login_timestamp`)
 VALUES
   (NULL, '1', NULL, 'demo', 'demo@demo.com', '$2y$10$EwF6/cRU8hVn8b7j.WswyeLmie6oYOI83ljS3WvWW9uu4GOzevOOy', '0', NULL, NULL, NULL),
   (NULL, '1', NULL, 'demo2', 'demo2@demo.com', '$2y$10$EwF6/cRU8hVn8b7j.WswyeLmie6oYOI83ljS3WvWW9uu4GOzevOOy', '0', NULL, NULL, NULL);
 
-DELETE FROM `_contests`;
+TRUNCATE TABLE `_contests`;
 /*!40000 ALTER TABLE `_contests` DISABLE KEYS */;
 INSERT INTO `_contests` (`id`, `game_id`, `contest_type_id`, `name`, `description`, `banner_url`, `begins_at`, `ends_at`, `display_ad`) VALUES
 	(1, 0, 0, NULL, NULL, NULL, '2018-01-29 00:00:00', '2018-01-29 00:00:00', 0),
