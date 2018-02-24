7d3e375ff11d1d1280fab4fe0ff02300d7968db4
Rafal Godawa <rgodawa@gmail.com>
2018-02-24 17:32:53 +0100

    url dla windowsa

--- public/index.php
+++ public/index.php
@@ -10,6 +10,8 @@ function e(String $value) {
 
 App\Config::setEnvironment(getenv('APPLICATION_ENV'));
 
+if (!isset($_GET['url'])) $_GET['url'] = '/';
+
 $router = new Core\Router($_GET['url']);
 $router->dispatch();
 
