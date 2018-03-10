d0203b7b367bae1d4858fe5e44e61f8dfecc1d71
Jakub Godawa <jakub.godawa@gmail.com>
2018-03-10 20:31:27 +0530

    readme and user/admin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Konkursiaki
+
+## Setup
+
+- check app/lib/config.php
+- run `php db/reset.php`
+- point web server at ksiaki/app/
+
+## login
+
+user
+12345678
+
+admin
+12345678
+
+## To do
+
+remembered_logins
+mailer
+...
--- db/migrations/999-seed.sql
+++ db/migrations/999-seed.sql
@@ -56,8 +56,8 @@ TRUNCATE TABLE `_users`;
 /*!40000 ALTER TABLE `remembered_logins` DISABLE KEYS */;
 INSERT INTO `_users` (`id`, `role_id`, `session_id`, `name`, `email`, `password_hash`, `active`, `activation_hash`, `created_at`, `last_login_at`)
 VALUES
-(1, 1, NULL, 'demo', 'demo@demo.com', '$2y$10$EwF6/cRU8hVn8b7j.WswyeLmie6oYOI83ljS3WvWW9uu4GOzevOOy', '0', NULL, NULL, NULL),
-(2, 2, NULL, 'demo2', 'demo2@demo.com', '$2y$10$EwF6/cRU8hVn8b7j.WswyeLmie6oYOI83ljS3WvWW9uu4GOzevOOy', '0', NULL, NULL, NULL);
+(1, 1, NULL, 'user', 'demo@demo.com', '$2y$10$EwF6/cRU8hVn8b7j.WswyeLmie6oYOI83ljS3WvWW9uu4GOzevOOy', '0', NULL, NULL, NULL),
+(2, 2, NULL, 'admin', 'demo2@demo.com', '$2y$10$EwF6/cRU8hVn8b7j.WswyeLmie6oYOI83ljS3WvWW9uu4GOzevOOy', '0', NULL, NULL, NULL);
 
 TRUNCATE TABLE `_contests`;
 /*!40000 ALTER TABLE `_contests` DISABLE KEYS */;
