8b8ee6831d6e8ce121a351bd6fdffb1e62cc9404
Jakub Godawa <jakub.godawa@gmail.com>
2018-06-13 13:21:54 +0200

    aiming for automated deployment, in progress

---
+++ deploy.php
@@ -0,0 +1,57 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'konkursiaki');
+
+// Project repository
+set('repository', 'git@bitbucket.org:rgodawa/ksiaki.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', false);
+
+// Shared files/dirs between deploys
+set('shared_files', []);
+set('shared_dirs', []);
+
+// Writable dirs by web server
+set('writable_dirs', []);
+
+
+// Hosts
+
+host('ksiaki')
+    ->stage('production')
+    ->set('deploy_path', '/var/www/{{application}}');
+
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+task('pwd', function () {
+    $result = run('pwd');
+    writeln("Current dir: $result");
+});
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+set('allow_anonymous_stats', true);
