9cd9bcfb44d64c4f6f9e55caea961394459c9b62
Jakub Godawa <jakub.godawa@gmail.com>
2018-06-15 14:18:29 +0200

    contest header

--- app/contest.php
+++ app/contest.php
@@ -4,7 +4,9 @@ include 'init.php';
 
 $params = [
     "offset" => 0,
-    "limit" => 10
+    "limit" => 10,
+    "month" => date('Y-m-01'),
+    "year" => date('Y-m-d')
 ];
 
 $params = array_merge($params, $_GET);
@@ -21,6 +23,26 @@ $data['other_contests'] = execute('call sp_contests_all_but_one(:p_id);', array(
     array('p_id', $data['contest']->id, PDO::PARAM_INT)
 ), true);
 
+$data['monthly_ranking'] = execute('call sp_rankings_monthly(
+  :p_date,
+  :p_offset,
+  :p_limit
+);', array(
+  array('p_date', $params['month'], PDO::PARAM_STR),
+  array('p_offset', $params['offset'], PDO::PARAM_INT),
+  array('p_limit', $params['limit'], PDO::PARAM_INT)
+), true);
+
+$data['yearly_ranking'] = execute('call sp_rankings_yearly(
+  :p_date,
+  :p_offset,
+  :p_limit
+);', array(
+  array('p_date', $params['year'], PDO::PARAM_STR),
+  array('p_offset', $params['offset'], PDO::PARAM_INT),
+  array('p_limit', $params['limit'], PDO::PARAM_INT)
+), true);
+
 $data['contest_ranking'] = execute('call sp_rankings_contest(
     :p_contest_id,
     :p_offset,
@@ -34,20 +56,27 @@ $data['contest_ranking'] = execute('call sp_rankings_contest(
 function content($params, $data) { ?>
 
 <div id="contests-page">
-  <div class="wrapper">
-
-  <h1 class="center"><?= $data['contest']->name ?></h1>
-  <p class="center"><?= t($data['game']->description) ?></p>
-
-  <div class="wrapper">
+  <div class="full-wrapper contest-header" style="background: url('<?= $data['contest']->header_url ?>') center top no-repeat">
+    <h1 class="center"><?= $data['contest']->name ?></h1>
+    <p class="center"><?= t($data['game']->description) ?></p>
     <?php if ($data['contest']->display_ad) { ?>
         <h2 class="center"><?= link_to(t('play'), t('contest_preroll_slug', ['slug' => $data['contest']->slug])) ?></h2>
     <?php } else { ?>
         <h2 class="center"><?= link_to(t('play'), t('contest_play_slug', ['slug' => $data['contest']->slug])) ?></h2>
     <?php } ?>
+  </div>
+
+  <div class="wrapper">
+
     <div class="side rankings">
       <h2><?= link_to(t('contest_ranking', ['name' => $data['contest']->name]), '/rankings/index.php?type=contest&date=' . $data['contest']->id) ?></h2>
       <?= ranking_list($data['contest_ranking']) ?>
+
+      <h2><?= link_to(t('monthly_ranking'), '/rankings/index.php?type=monthly&date=' . $params['month']) ?></h2>
+      <?= ranking_list($data['monthly_ranking']) ?>
+
+      <h2><?= link_to(t('yearly_ranking'), '/rankings/index.php?type=yearly&date=' . $params['year']) ?></h2>
+      <?= ranking_list($data['yearly_ranking']) ?>
     </div>
 
     <?php include './partials/modal_ranking.html' ?>
