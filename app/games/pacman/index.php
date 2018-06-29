15c82fd62ebb7b6200de41d30190db08175cfd9c
Jakub Godawa <jakub.godawa@gmail.com>
2018-06-29 18:33:47 +0200

    serve fresh

--- app/games/pacman/index.php
+++ app/games/pacman/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8" />
     <title>HTML5 Pacman</title>
     <link rel="stylesheet" href="css/pacman.css" />
-    <script src="js/pacman.min.js"></script>
+    <script src="js/pacman.min.js?v=<?php echo filemtime("js/pacman.min.js") ?>"></script>
 
 </head>
 
