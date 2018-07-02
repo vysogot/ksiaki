716b660ea7abe6ddf010aed587a7847710564df2
Jakub Godawa <jakub.godawa@gmail.com>
2018-07-02 10:11:16 +0200

    podmiana konkursiaka miesiaca

--- app/partials/app_sky_banner.php
+++ app/partials/app_sky_banner.php
@@ -1,4 +1,4 @@
 <aside class="sky-banner">
-	<?= link_to(image('/assets/images/konkursiak-miesiaca.png'), '/assets/images/nagroda-konkursiaka.png', ['class' => 'modal-image']) ?>
+	<?= link_to(image('/assets/images/konkursiak-miesiaca.png'), '/assets/images/nagroda-konkursiaka-lipiec.png', ['class' => 'modal-image']) ?>
 	<?= link_to(image('/assets/images/sky_banner_batman.jpg'), t('hero_slug', ['slug' => 'lego-batman'])) ?>
 </aside>
