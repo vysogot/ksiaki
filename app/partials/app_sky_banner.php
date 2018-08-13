af913290eafe3160d265b47740fe97eab7caec30
Jakub Godawa <jakub.godawa@gmail.com>
2018-08-13 13:57:37 +0200

    lego city link

--- app/partials/app_sky_banner.php
+++ app/partials/app_sky_banner.php
@@ -1,2 +1,2 @@
 <?= link_to(image('/assets/images/konkursiak-miesiaca-08v2.png', ['style' => 'width: 100%;']), '/assets/images/konkursiak-nagroda-08.png', ['class' => 'modal-image']) ?>
-<?= link_to(image('/assets/images/sky_banner_lego_city.jpg'), t('hero_slug', ['slug' => 'lego-batman'])) ?>
+<?= link_to(image('/assets/images/sky_banner_lego_city.jpg'), t('hero_slug', ['slug' => 'lego-city'])) ?>
